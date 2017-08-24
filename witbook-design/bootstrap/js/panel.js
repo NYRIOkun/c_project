ef3a12e45d2c2b1ff60b2949522de228611691c5
Taku Nagashima <taku.nagashima4112@gmail.com>
2017-08-24 05:54:13 +0000

    add : panel_html with jquery

--- witbook-design/bootstrap/js/panel.js
+++ witbook-design/bootstrap/js/panel.js
@@ -1,4 +1,20 @@
 
 $(function(){
-
+    $(".main_showcase_img > img").hover(
+        function(){
+           $(this). css("opacity","0.5");
+        },
+        function(){
+           $(this). css("opacity","1"); 
+        });
+    
+    $(".slidetext").hover(
+        function(){
+           $(this).addClass("slidetoright");
+        },
+        function(){
+           $(this).removeClass("slidetoright");
+        });
+    
+    
 });
